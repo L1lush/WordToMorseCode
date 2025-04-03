32772064097d238a1737ce518ac96d02c06eab9b
bdm77 <bdm77@DESKTOP-5C808VG>
2025-04-03 16:52:07 +0300

    Add project files.

---
+++ WordToMorse/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WordToMorse
+{
+	class MorseCodeConverter
+	{
+		static void Main()
+		{
+			Dictionary<char, string> morseCodeDict = new Dictionary<char, string>() // create dictionary  
+		{
+			{'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."}, {'F', "..-."},
+			{'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
+			{'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."},
+			{'S', "..."}, {'T', "-"}, {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
+			{'Y', "-.--"}, {'Z', "--.."},
+			{'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"},
+			{'5', "....."}, {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}
+		};
+
+			Console.Write("Enter your phrase ");
+			string input = Console.ReadLine().ToUpper();
+
+			string morseCode = ConvertToMorseCode(input, morseCodeDict);
+			Console.WriteLine($"Morse Code: {morseCode}");
+
+			PlayMorseCode(morseCode);
+		}
+
+		static string ConvertToMorseCode(string input, Dictionary<char, string> morseCodeDict) // convert word to morse code
+		{
+			string morseCode = "";
+			int i = 0;
+
+			while (i < input.Length)
+			{
+				char c = input[i];
+				if (morseCodeDict.ContainsKey(c))
+				{
+					morseCode += morseCodeDict[c] + " ";
+				}
+				else if (c == ' ')
+				{
+					morseCode += "/ ";
+				}
+				i++;
+			}
+
+			return morseCode.Trim();
+		}
+
+		static void PlayMorseCode(string morseCode)
+		{
+			foreach (char symbol in morseCode)
+			{
+				if (symbol == '.')
+				{
+					// Short beep for dot
+					Console.Beep(1000, 200);
+					Thread.Sleep(200);
+				}
+				else if (symbol == '-')
+				{
+					// Long beep for dash
+					Console.Beep(1000, 600);
+					Thread.Sleep(200);
+				}
+				else if (symbol == ' ')
+				{
+					// Pause between characters
+					Thread.Sleep(200);
+				}
+				else if (symbol == '/')
+				{
+					// Pause between words
+					Thread.Sleep(1000);
+				}
+			}
+		}
+	}
+}
---
+++ WordToMorse/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WordToMorse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordToMorse")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0d10825-2ec9-4eee-b82e-7c4c271851a6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
